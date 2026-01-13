0670cbde8c7607a508491973d3df5752f44a6982
Mikael Siidorow <mikael@siidorow.com>
2026-01-13 21:23:00 +0200

    set up remote tf backend via r2

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,5 +1,21 @@
 terraform {
   required_version = ">= 1.12.2"
+
+  backend "s3" {
+    bucket                      = "terraform-state"
+    key                         = "terraform.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    endpoints = {
+      s3 = "https://0d7cd4f74493972b3d64775916c9f6ed.eu.r2.cloudflarestorage.com"
+    }
+  }
+
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
